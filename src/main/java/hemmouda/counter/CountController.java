5e3dc0a5d30dc7da28d9a03bcfae0a6350af3ecf
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-10-11 18:01:58 +0200

    Update CountController.java
    Using EntityModel

--- src/main/java/hemmouda/counter/CountController.java
+++ src/main/java/hemmouda/counter/CountController.java
@@ -1,23 +1,48 @@
 package hemmouda.counter;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import java.util.List;
 
 @RestController
 public class CountController {
 
-    private final CountRepo repo;
+    private final CountRepository repo;
+    private final CountModelAssembler assembler;
 
-    CountController(CountRepo repository) {
+    CountController(CountRepository repository, CountModelAssembler assembler) {
         this.repo = repository;
+        this.assembler = assembler;
     }
 
 
     @GetMapping("/counts")
-    List<Count> all() {
-        return repo.findAll();
+    CollectionModel<EntityModel<Count>> all() {
+        List<EntityModel<Count>> counts = repo.findAll().stream()
+                .map(assembler::toModel)
+                .toList();
+
+        return CollectionModel.of(counts, linkTo(methodOn(CountController.class).all()).withSelfRel());
+    }
+
+    @GetMapping("/counts/{name}")
+    EntityModel<Count> one(@PathVariable String name) {
+
+        Count count = repo.findById(name)
+                .orElseGet(() -> new Count(name));
+
+        return assembler.toModel(count);
+    }
+
+    @DeleteMapping("/counts/{name}")
+    void delete(@PathVariable String name) {
+        repo.deleteById(name);
     }
 
 }
