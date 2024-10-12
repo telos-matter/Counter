ad084891c838df3a0d694465026080d41de8c82d
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-10-12 13:49:19 +0200

    Create CountNotFoundAdvice.java

---
+++ src/main/java/hemmouda/counter/CountNotFoundAdvice.java
@@ -0,0 +1,17 @@
+package hemmouda.counter;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CountNotFoundAdvice {
+
+    @ExceptionHandler(CountNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String countNotFoundHandler (CountNotFoundException e) {
+        return e.getMessage();
+    }
+
+}
