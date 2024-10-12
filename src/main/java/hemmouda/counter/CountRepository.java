9f0b9f9a210e815390f14cde6306f08f1cc9d26d
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-10-12 13:49:23 +0200

    Update CountRepository.java

--- src/main/java/hemmouda/counter/CountRepository.java
+++ src/main/java/hemmouda/counter/CountRepository.java
@@ -1,7 +1,16 @@
 package hemmouda.counter;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface CountRepository extends MongoRepository<Count, String> {
+
+    /**
+     * @return a Count document with the name <code>name</code>
+     * if found, otherwise <code>null</code>. Case insensitive
+     * search.
+     */
+    @Query("{ 'name': { $regex: ?0, $options: 'i' } }")
+    Count findByName (String name) ;
 
 }
